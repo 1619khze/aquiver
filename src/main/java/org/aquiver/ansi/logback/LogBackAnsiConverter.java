0dbb88b9f35790dc040b45a7c7cc5b03a90002d0
1619kHz <1517262320@qq.com>
2020-02-27 21:43:40 +0800

    :sparkles: 基于logback增加springboot风格日志

---
+++ src/main/java/org/aquiver/ansi/logback/LogBackAnsiConverter.java
@@ -0,0 +1,48 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 1619kHz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.aquiver.ansi.logback;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.pattern.color.ANSIConstants;
+import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase;
+
+public class LogBackAnsiConverter extends ForegroundCompositeConverterBase<ILoggingEvent> {
+
+  @Override
+  protected String getForegroundColorCode(ILoggingEvent iLoggingEvent) {
+    switch (iLoggingEvent.getLevel().toInt()) {
+      case Level.ERROR_INT:
+        return ANSIConstants.RED_FG;
+      case Level.WARN_INT:
+        return ANSIConstants.YELLOW_FG;
+      case Level.INFO_INT:
+        return ANSIConstants.GREEN_FG;
+      case Level.DEBUG_INT:
+        return ANSIConstants.MAGENTA_FG;
+      default:
+        return null;
+    }
+  }
+}
