01af76e017fc2691686ace13a122b132fba9bc02
1619kHz <1517262320@qq.com>
2020-02-27 21:41:57 +0800

    :sparkles: websocket简单支持

---
+++ src/main/java/org/aquiver/server/websocket/WebSocketServerInitializer.java
@@ -0,0 +1,56 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 1619kHz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.aquiver.server.websocket;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketServerCompressionHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @author WangYi
+ * @since 2019/6/17
+ */
+public class WebSocketServerInitializer extends ChannelInitializer<SocketChannel> {
+
+  private final String webSocketPath;
+
+  public WebSocketServerInitializer(String webSocketPath) {
+    this.webSocketPath = webSocketPath;
+  }
+
+  @Override
+  protected void initChannel(SocketChannel socketChannel) {
+    ChannelPipeline pipeline = socketChannel.pipeline();
+    pipeline.addLast(new HttpServerCodec());
+    pipeline.addLast(new ChunkedWriteHandler());
+    pipeline.addLast(new HttpObjectAggregator(65536));
+    pipeline.addLast(new WebSocketServerCompressionHandler());
+    pipeline.addLast(new WebSocketServerProtocolHandler(webSocketPath, null, true));
+  }
+}
