f8c87e0735bda13b8b47ceda69039f2d838a7562
everknwon <1517262320@qq.com>
2020-04-25 15:21:57 +0800

    :art: 更新License和增加mediaType属性

--- src/main/java/org/aquiver/Response.java
+++ src/main/java/org/aquiver/Response.java
@@ -1,10 +1,36 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 1619kHz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.aquiver;
+
+import org.aquiver.mvc.MediaType;
 
 import java.util.Objects;
 
 public class Response {
-  private Object  result;
-  private boolean jsonResponse;
+  private Object    result;
+  private boolean   jsonResponse;
+  private MediaType mediaType;
 
   public Response(Object result, boolean jsonResponse) {
     this.result       = result;
@@ -27,11 +53,20 @@ public class Response {
     this.jsonResponse = jsonResponse;
   }
 
+  public MediaType getMediaType() {
+    return mediaType;
+  }
+
+  public void setMediaType(MediaType mediaType) {
+    this.mediaType = mediaType;
+  }
+
   @Override
   public String toString() {
     return "Response{" +
             "result=" + result +
             ", jsonResponse=" + jsonResponse +
+            ", mediaType=" + mediaType +
             '}';
   }
 
@@ -41,11 +76,12 @@ public class Response {
     if (o == null || getClass() != o.getClass()) return false;
     Response response = (Response) o;
     return jsonResponse == response.jsonResponse &&
-            Objects.equals(result, response.result);
+            Objects.equals(result, response.result) &&
+            Objects.equals(mediaType, response.mediaType);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(result, jsonResponse);
+    return Objects.hash(result, jsonResponse, mediaType);
   }
 }
