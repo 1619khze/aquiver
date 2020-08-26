95583e63278997a1d2e9672cd7c3e99ac1632053
1619kHz <37778292+everknwon@users.noreply.github.com>
2020-08-26 14:13:33 +0800

    ♻️ 支持拦截器

---
+++ src/main/java/org/aquiver/mvc/interceptor/AspectInterceptor.java
@@ -0,0 +1,43 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 1619kHz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.aquiver.mvc.interceptor;
+
+import org.aquiver.RequestContext;
+
+/**
+ * @author WangYi
+ * @since 2020/8/26
+ */
+public abstract class AspectInterceptor implements Interceptor {
+  protected abstract void before(RequestContext context);
+
+  protected abstract void after(RequestContext context);
+
+  @Override
+  public void intercept(RequestContext ctx, InterceptorChain chain) throws Exception {
+    before(ctx);
+    chain.invoke();
+    after(ctx);
+  }
+}
