d147cb6a37f3895bc97146df3d68d137249573d9
1619kHz <37778292+everknwon@users.noreply.github.com>
2020-08-31 20:33:51 +0800

    ♻️ 支持使用JSON注解返回json

---
+++ src/main/java/org/aquiver/result/JsonResultHandler.java
@@ -0,0 +1,36 @@
+package org.aquiver.result;
+
+import com.alibaba.fastjson.JSONObject;
+import io.netty.handler.codec.http.DefaultHttpHeaders;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaders;
+import org.aquiver.RequestContext;
+import org.aquiver.ResultHandler;
+import org.aquiver.mvc.RequestResult;
+import org.aquiver.mvc.annotation.JSON;
+import org.aquiver.mvc.http.MediaType;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author WangYi
+ * @since 2020/8/31
+ */
+public class JsonResultHandler implements ResultHandler {
+
+  @Override
+  public boolean support(RequestResult requestResult) {
+    Method method = requestResult.getMethod();
+    return method.isAnnotationPresent(JSON.class);
+  }
+
+  @Override
+  public void handle(RequestContext ctx, RequestResult result) throws Exception {
+    String jsonString = JSONObject.toJSONString(result.getResultObject());
+    HttpHeaders httpHeaders = new DefaultHttpHeaders();
+    httpHeaders.add(HttpHeaderNames.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+    FullHttpResponse jsonResponse = ResultUtils.contentResponse(jsonString);
+    ctx.writeAndFlush(jsonResponse);
+  }
+}
