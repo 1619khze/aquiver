fa386078318092f6968d47b6f0cd881bc7f52e52
1619kHz <37778292+everknwon@users.noreply.github.com>
2020-10-21 16:02:01 +0800

    ♻️ 封装Http状态码

--- src/main/java/org/aquiver/mvc/http/HttpStatus.java
+++ src/main/java/org/aquiver/mvc/http/HttpStatus.java
@@ -29,66 +29,50 @@ import io.netty.handler.codec.http.HttpResponseStatus;
  * @author WangYi
  * @since 2020/8/22
  */
-public final class HttpStatus {
-  public static final HttpStatus CONTINUE = new HttpStatus(HttpResponseStatus.CONTINUE.code());
-  public static final HttpStatus SWITCHING_PROTOCOLS = new HttpStatus(HttpResponseStatus.SWITCHING_PROTOCOLS.code());
-  public static final HttpStatus OK = new HttpStatus(HttpResponseStatus.OK.code());
-  public static final HttpStatus CREATED = new HttpStatus(HttpResponseStatus.CREATED.code());
-  public static final HttpStatus ACCEPTED = new HttpStatus(HttpResponseStatus.ACCEPTED.code());
-  public static final HttpStatus NON_AUTHORITATIVE_INFORMATION = new HttpStatus(HttpResponseStatus.NON_AUTHORITATIVE_INFORMATION.code());
-  public static final HttpStatus NO_CONTENT = new HttpStatus(HttpResponseStatus.NO_CONTENT.code());
-  public static final HttpStatus RESET_CONTENT = new HttpStatus(HttpResponseStatus.RESET_CONTENT.code());
-  public static final HttpStatus PARTIAL_CONTENT = new HttpStatus(HttpResponseStatus.PARTIAL_CONTENT.code());
-  public static final HttpStatus MULTIPLE_CHOICES = new HttpStatus(HttpResponseStatus.MULTIPLE_CHOICES.code());
-  public static final HttpStatus MOVED_PERMANENTLY = new HttpStatus(HttpResponseStatus.MOVED_PERMANENTLY.code());
-  public static final HttpStatus MOVED_TEMPORARILY = new HttpStatus(HttpResponseStatus.FOUND.code());
-  public static final HttpStatus FOUND = new HttpStatus(HttpResponseStatus.FOUND.code());
-  public static final HttpStatus SEE_OTHER = new HttpStatus(HttpResponseStatus.SEE_OTHER.code());
-  public static final HttpStatus NOT_MODIFIED = new HttpStatus(HttpResponseStatus.NOT_MODIFIED.code());
-  public static final HttpStatus USE_PROXY = new HttpStatus(HttpResponseStatus.USE_PROXY.code());
-  public static final HttpStatus TEMPORARY_REDIRECT = new HttpStatus(HttpResponseStatus.TEMPORARY_REDIRECT.code());
-  public static final HttpStatus BAD_REQUEST = new HttpStatus(HttpResponseStatus.BAD_REQUEST.code());
-  public static final HttpStatus UNAUTHORIZED = new HttpStatus(HttpResponseStatus.UNAUTHORIZED.code());
-  public static final HttpStatus PAYMENT_REQUIRED = new HttpStatus(HttpResponseStatus.PAYMENT_REQUIRED.code());
-  public static final HttpStatus FORBIDDEN = new HttpStatus(HttpResponseStatus.FORBIDDEN.code());
-  public static final HttpStatus NOT_FOUND = new HttpStatus(HttpResponseStatus.NOT_FOUND.code());
-  public static final HttpStatus METHOD_NOT_ALLOWED = new HttpStatus(HttpResponseStatus.METHOD_NOT_ALLOWED.code());
-  public static final HttpStatus NOT_ACCEPTABLE = new HttpStatus(HttpResponseStatus.NOT_ACCEPTABLE.code());
-  public static final HttpStatus PROXY_AUTHENTICATION_REQUIRED = new HttpStatus(HttpResponseStatus.PROXY_AUTHENTICATION_REQUIRED.code());
-  public static final HttpStatus REQUEST_TIMEOUT = new HttpStatus(HttpResponseStatus.REQUEST_TIMEOUT.code());
-  public static final HttpStatus CONFLICT = new HttpStatus(HttpResponseStatus.CONFLICT.code());
-  public static final HttpStatus GONE = new HttpStatus(HttpResponseStatus.GONE.code());
-  public static final HttpStatus LENGTH_REQUIRED = new HttpStatus(HttpResponseStatus.LENGTH_REQUIRED.code());
-  public static final HttpStatus PRECONDITION_FAILED = new HttpStatus(HttpResponseStatus.PRECONDITION_FAILED.code());
-  public static final HttpStatus REQUEST_ENTITY_TOO_LARGE = new HttpStatus(HttpResponseStatus.REQUEST_ENTITY_TOO_LARGE.code());
-  public static final HttpStatus REQUEST_URI_TOO_LONG = new HttpStatus(HttpResponseStatus.REQUEST_URI_TOO_LONG.code());
-  public static final HttpStatus UNSUPPORTED_MEDIA_TYPE = new HttpStatus(HttpResponseStatus.UNSUPPORTED_MEDIA_TYPE.code());
-  public static final HttpStatus REQUESTED_RANGE_NOT_SATISFIABLE = new HttpStatus(HttpResponseStatus.REQUESTED_RANGE_NOT_SATISFIABLE.code());
-  public static final HttpStatus EXPECTATION_FAILED = new HttpStatus(HttpResponseStatus.EXPECTATION_FAILED.code());
-  public static final HttpStatus INTERNAL_SERVER_ERROR = new HttpStatus(HttpResponseStatus.INTERNAL_SERVER_ERROR.code());
-  public static final HttpStatus NOT_IMPLEMENTED = new HttpStatus(HttpResponseStatus.NOT_IMPLEMENTED.code());
-  public static final HttpStatus BAD_GATEWAY = new HttpStatus(HttpResponseStatus.BAD_GATEWAY.code());
-  public static final HttpStatus SERVICE_UNAVAILABLE = new HttpStatus(HttpResponseStatus.SERVICE_UNAVAILABLE.code());
-  public static final HttpStatus GATEWAY_TIMEOUT = new HttpStatus(HttpResponseStatus.GATEWAY_TIMEOUT.code());
-  public static final HttpStatus HTTP_VERSION_NOT_SUPPORTED = new HttpStatus(HttpResponseStatus.HTTP_VERSION_NOT_SUPPORTED.code());
-  private final int status;
-  private final String message;
-
-  public HttpStatus(int status) {
-    this.status = status;
-    this.message = null;
-  }
-
-  public HttpStatus(int status, String message) {
-    this.status = status;
-    this.message = message;
-  }
-
-  public int getStatus() {
-    return status;
-  }
-
-  public String getMessage() {
-    return message;
-  }
+public interface HttpStatus {
+  int CONTINUE = HttpResponseStatus.CONTINUE.code();
+  int SWITCHING_PROTOCOLS = HttpResponseStatus.SWITCHING_PROTOCOLS.code();
+  int OK = HttpResponseStatus.OK.code();
+  int CREATED = HttpResponseStatus.CREATED.code();
+  int ACCEPTED = HttpResponseStatus.ACCEPTED.code();
+  int NON_AUTHORITATIVE_INFORMATION = HttpResponseStatus.NON_AUTHORITATIVE_INFORMATION.code();
+  int NO_CONTENT = HttpResponseStatus.NO_CONTENT.code();
+  int RESET_CONTENT = HttpResponseStatus.RESET_CONTENT.code();
+  int PARTIAL_CONTENT = HttpResponseStatus.PARTIAL_CONTENT.code();
+  int MULTIPLE_CHOICES = HttpResponseStatus.MULTIPLE_CHOICES.code();
+  int MOVED_PERMANENTLY = HttpResponseStatus.MOVED_PERMANENTLY.code();
+  int MOVED_TEMPORARILY = HttpResponseStatus.FOUND.code();
+  int FOUND = HttpResponseStatus.FOUND.code();
+  int SEE_OTHER = HttpResponseStatus.SEE_OTHER.code();
+  int NOT_MODIFIED = HttpResponseStatus.NOT_MODIFIED.code();
+  int USE_PROXY = HttpResponseStatus.USE_PROXY.code();
+  int TEMPORARY_REDIRECT = HttpResponseStatus.TEMPORARY_REDIRECT.code();
+  int BAD_REQUEST = HttpResponseStatus.BAD_REQUEST.code();
+  int UNAUTHORIZED = HttpResponseStatus.UNAUTHORIZED.code();
+  int PAYMENT_REQUIRED = HttpResponseStatus.PAYMENT_REQUIRED.code();
+  int FORBIDDEN = HttpResponseStatus.FORBIDDEN.code();
+  int NOT_FOUND = HttpResponseStatus.NOT_FOUND.code();
+  int METHOD_NOT_ALLOWED = HttpResponseStatus.METHOD_NOT_ALLOWED.code();
+  int NOT_ACCEPTABLE = HttpResponseStatus.NOT_ACCEPTABLE.code();
+  int PROXY_AUTHENTICATION_REQUIRED = HttpResponseStatus.PROXY_AUTHENTICATION_REQUIRED.code();
+  int REQUEST_TIMEOUT = HttpResponseStatus.REQUEST_TIMEOUT.code();
+  int CONFLICT = HttpResponseStatus.CONFLICT.code();
+  int GONE = HttpResponseStatus.GONE.code();
+  int LENGTH_REQUIRED = HttpResponseStatus.LENGTH_REQUIRED.code();
+  int PRECONDITION_FAILED = HttpResponseStatus.PRECONDITION_FAILED.code();
+  int REQUEST_ENTITY_TOO_LARGE = HttpResponseStatus.REQUEST_ENTITY_TOO_LARGE.code();
+  int REQUEST_URI_TOO_LONG = HttpResponseStatus.REQUEST_URI_TOO_LONG.code();
+  int UNSUPPORTED_MEDIA_TYPE = HttpResponseStatus.UNSUPPORTED_MEDIA_TYPE.code();
+  int REQUESTED_RANGE_NOT_SATISFIABLE = HttpResponseStatus.REQUESTED_RANGE_NOT_SATISFIABLE.code();
+  int EXPECTATION_FAILED = HttpResponseStatus.EXPECTATION_FAILED.code();
+  int INTERNAL_SERVER_ERROR = HttpResponseStatus.INTERNAL_SERVER_ERROR.code();
+  int NOT_IMPLEMENTED = HttpResponseStatus.NOT_IMPLEMENTED.code();
+  int BAD_GATEWAY = HttpResponseStatus.BAD_GATEWAY.code();
+  int SERVICE_UNAVAILABLE = HttpResponseStatus.SERVICE_UNAVAILABLE.code();
+  int GATEWAY_TIMEOUT = HttpResponseStatus.GATEWAY_TIMEOUT.code();
+  int HTTP_VERSION_NOT_SUPPORTED = HttpResponseStatus.HTTP_VERSION_NOT_SUPPORTED.code();
+  int VARIANT_ALSO_NEGOTIATES = HttpResponseStatus.VARIANT_ALSO_NEGOTIATES.code();
+  int INSUFFICIENT_STORAGE = HttpResponseStatus.INSUFFICIENT_STORAGE.code();
+  int NOT_EXTENDED = HttpResponseStatus.NOT_EXTENDED.code();
+  int NETWORK_AUTHENTICATION_REQUIRED = HttpResponseStatus.NETWORK_AUTHENTICATION_REQUIRED.code();
 }
